6d82ccf0c00f39c92ac8f377330d5a9f56967642
Martin Schorb <schorb@embl.de>
2022-02-07 15:20:06 +0100

    add SBEMImage test data

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-![Build Status](https://schorb.embl-community.io/badges/tests/coverage.svg)
-
+![Build Status](https://git.embl.de/schorb/rendermodules-addons/badges/tests/coverage.svg)
+![Pipeline status](https://git.embl.de/schorb/rendermodules-addons/badges/tests/pipeline.svg)
 ---
+# This repository contains additional modules for [`asap-modules`](https://github.com/AllenInstitute/asap-modules)
 
+### [package documentation](https://schorb.embl-community.io/rendermodules-addons)
--- tests/test_dataimport.py
+++ tests/test_dataimport.py
@@ -1,20 +1,22 @@
 import json
-from six.moves import urllib
-from six import viewkeys, iteritems
 import tempfile
-import logging
 import pytest
-import renderapi
-import marshmallow as mm
-from asap.utilities.pillow_utils import Image
-from asap.module.render_module import RenderModuleException
-from asap.dataimport import generate_EM_tilespecs_from_metafile
-from asap.dataimport import generate_mipmaps
-from asap.dataimport import apply_mipmaps_to_render
-from test_data import (render_params,
-                       )
-import os
 import copy
+import os
+
+import renderapi
+from marshmallow.exceptions import ValidationError
+
+from asap.module.render_module import RenderModuleException
+# from asap.dataimport import generate_EM_tilespecs_from_metafile
+# from asap.dataimport import generate_mipmaps
+# from asap.dataimport import apply_mipmaps_to_render
+
+from rmaddons.dataimport import generate_EM_tilespecs_from_SBEMImage
+
+from test_data import (render_params,
+                       example_sbem)
+
 
 
 @pytest.fixture(scope='module')
@@ -22,17 +24,40 @@ def render():
     return renderapi.connect(**render_params)
 
 def test_generate_SBEM(render):
-    print(render_params)
+    # print(render_params)
     # os.system('ping '+render_params["host"] + ' -c 3')
-    # assert isinstance(render, renderapi.render.RenderClient)
+    assert isinstance(render, renderapi.render.Render)
 
-#     with open(METADATA_FILE, 'r') as f:
-#         md = json.load(f)
-#     ex = copy.deepcopy(generate_EM_tilespecs_from_metafile.example_input)
-#     ex['render'] = render.make_kwargs()
-#     ex['metafile'] = METADATA_FILE
-#     with tempfile.NamedTemporaryFile(suffix='.json') as probablyemptyfn:
-#         outfile = probablyemptyfn.name
+    ex = copy.deepcopy(generate_EM_tilespecs_from_SBEMImage.example_input)
+    ex['render'] = render.make_kwargs()
+
+
+    with tempfile.NamedTemporaryFile(suffix='.json') as probablyemptyfn:
+        outfile = probablyemptyfn.name
+
+    # test non existing directory
+    ex['image_directory'] = example_sbem + 'notexistingdir'
+
+    with pytest.raises(ValidationError):
+        mod1 = generate_EM_tilespecs_from_SBEMImage.GenerateSBEMImageTileSpecs(input_data=ex,
+                                                                           args=['--output_json', outfile])
+
+    ex['image_directory'] = example_sbem
+
+    # test non existing "meta" directory
+    os.rename(example_sbem + '/meta', example_sbem + '/meta123')
+
+    mod = generate_EM_tilespecs_from_SBEMImage.GenerateSBEMImageTileSpecs(input_data=ex,
+                                                                           args=['--output_json', outfile])
+    with pytest.raises(FileNotFoundError):
+        mod.run()
+
+    os.rename(example_sbem + '/meta123', example_sbem + '/meta')
+
+    mod.run()
+
+
+#
 #     mod = generate_EM_tilespecs_from_metafile.GenerateEMTileSpecsModule(
 #         input_data=ex, args=['--output_json', outfile])
 #     mod.run()
