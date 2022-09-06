429f325db98a116a9b07553de1be2f7b384343da
Martin Schorb <schorb@embl.de>
2022-09-06 16:30:21 +0200

    autcrop to FIBSEM importer tests

--- rmaddons/dataimport/generate_EM_tilespecs_from_TIFStack.py
+++ rmaddons/dataimport/generate_EM_tilespecs_from_TIFStack.py
@@ -93,11 +93,17 @@ class GenerateTifStackTileSpecs(StackOutputModule):
                 dtype = im.pages.pages[0].dtype.type
                 if autocrop:
                     image = im.asarray()
-                    r0, c0, r1, c1 = measure.regionprops(measure.label(image > 0))[0].bbox
-                    imcrop = image[r0:r1, c0:c1]
-                    width = c1 - c0
-                    height = r1 - r0
-                    transform = renderapi.transform.AffineModel(B0=r0, B1=c0)
+                    imcontent = np.argwhere(image != 0)  # assume blackground is zero
+                    min_x = imcontent[:, 0].min()
+                    max_x = imcontent[:, 0].max()
+                    min_y = imcontent[:, 1].min()
+                    max_y = imcontent[:, 1].max()
+
+
+                    imcrop = image[min_x:max_x+1, min_y:max_y+1]
+                    width = max_x - min_x
+                    height = max_y - min_y
+                    transform = renderapi.transform.AffineModel(B0=min_x, B1=min_y)
                     fdir = os.path.dirname(filepath)
                     if not os.path.exists(os.path.join(fdir,'autocrop')):
                         os.mkdir(os.path.join(fdir,'autocrop'))
--- tests/test_dataimport.py
+++ tests/test_dataimport.py
@@ -3,6 +3,7 @@ import pytest
 import copy
 import os
 import numpy as np
+from skimage import io
 
 import renderapi
 from marshmallow.exceptions import ValidationError
@@ -24,148 +25,148 @@ from test_data import (render_params,
 @pytest.fixture(scope='module')
 def render():
     return renderapi.connect(**render_params)
-
-@pytest.mark.dependency()
-def test_generate_SBEM(render):
-
-    assert isinstance(render, renderapi.render.Render)
-
-    ex = copy.deepcopy(generate_EM_tilespecs_from_SBEMImage.example_input)
-    ex['render'] = render.make_kwargs()
-
-    with tempfile.NamedTemporaryFile(suffix='.json') as probablyemptyfn:
-        outfile = probablyemptyfn.name
-
-    # test non existing directory
-    ex['image_directory'] = example_sbem + 'notexistingdir'
-
-    with pytest.raises(ValidationError):
-        mod1 = generate_EM_tilespecs_from_SBEMImage.GenerateSBEMImageTileSpecs(input_data=ex)
-
-    ex['image_directory'] = example_sbem
-
-    # test non existing "meta" directory
-    os.rename(example_sbem + '/meta', example_sbem + '/meta123')
-
-    mod = generate_EM_tilespecs_from_SBEMImage.GenerateSBEMImageTileSpecs(input_data=ex)
-
-    with pytest.raises(FileNotFoundError):
-        mod.run()
-
-    os.rename(example_sbem + '/meta123', example_sbem + '/meta')
-
-    # test missing tile image reporting to log
-    os.remove(example_sbem + '/tiles/g0000/t0008/test_example_g0000_t0008_s00005.jpg')
-
-    mod.run()
-
-    assert os.path.exists(example_sbem + '/conv_log')
-    with open(os.path.join(example_sbem, 'conv_log', os.listdir(example_sbem + '/conv_log')[0])) as file:
-        importlog = file.read()
-
-    assert importlog == sbemimage_template['errorlog0'] + example_sbem + sbemimage_template['errorlog1']
-
-    expected_tileIds = set(sbemimage_template['tileids'])
-    delivered_tileIds = set(renderapi.stack.get_stack_tileIds(ex['stack'], render=render))
-
-    # test if all tiles are imported
-    assert len(expected_tileIds.symmetric_difference(delivered_tileIds)) == 0
-
-    md = renderapi.stack.get_stack_metadata(render=render, stack=ex['stack'])
-
-    expected_resolution = sbemimage_template['resolution']
-    delivered_resolution = [md.stackResolutionX, md.stackResolutionY, md.stackResolutionZ]
-
-    # test if resolution of stack is correct
-    assert (np.array(expected_resolution) - np.array(delivered_resolution) == [0, 0, 0]).all()
-
-    # this is needed for testing an export -> moved to 'test_materialize'
-    # cleanup
-    # os.system('rm -rf ' + example_sbem)
-    # renderapi.stack.delete_stack(ex['stack'], render=render)
-
-
-@pytest.mark.dependency()
-def test_generate_SerialEM(render):
-
-    assert isinstance(render, renderapi.render.Render)
-
-    ex = copy.deepcopy(generate_EM_tilespecs_from_SerialEMmontage.example_input)
-    ex['render'] = render.make_kwargs()
-
-    with tempfile.NamedTemporaryFile(suffix='.json') as probablyemptyfn:
-        outfile = probablyemptyfn.name
-
-    # test non existing image file
-    ex['image_file'] = os.path.join(example_serialem, 'notexistingfile')
-
-    with pytest.raises(ValidationError):
-        mod1 = generate_EM_tilespecs_from_SerialEMmontage.GenerateSEMmontTileSpecs(input_data=ex)
-
-    ex['image_file'] = os.path.join(example_serialem, 'supermont.idoc')
-
-    mod = generate_EM_tilespecs_from_SerialEMmontage.GenerateSEMmontTileSpecs(input_data=ex)
-
-    stacks0 = mod.run()
-
-    assert len(stacks0) == 1
-
-    expected_stack0 = serialem_template['stack0']
-
-    assert stacks0[0] == expected_stack0
-
-    expected_tileIds = set(serialem_template['tileids0'])
-
-    delivered_tileIds = set(renderapi.stack.get_stack_tileIds(stacks0[0], render=render))
-
-    # test if all tiles are imported
-    assert len(expected_tileIds.symmetric_difference(delivered_tileIds)) == 0
-
-    md = renderapi.stack.get_stack_metadata(render=render, stack=stacks0[0])
-
-    expected_resolution = serialem_template['resolution0']
-    delivered_resolution = [md.stackResolutionX, md.stackResolutionY, md.stackResolutionZ]
-
-    # test if resolution of stack is correct
-    assert (np.array(expected_resolution) - np.array(delivered_resolution) == [0, 0, 0]).all()
-
-    ex['image_file'] = os.path.join(example_serialem, 'mont01.idoc')
-
-    mod = generate_EM_tilespecs_from_SerialEMmontage.GenerateSEMmontTileSpecs(input_data=ex)
-
-    stacks = mod.run()
-
-    assert len(stacks) == 5
-
-    assert stacks == serialem_template['stacks']
-
-    for stack in stacks:
-        expected_tileIds = set(serialem_template['tileids'][stack])
-
-        delivered_tileIds = set(renderapi.stack.get_stack_tileIds(stack, render=render))
-
-        # test if all tiles are imported
-        assert len(expected_tileIds.symmetric_difference(delivered_tileIds)) == 0
-
-        md = renderapi.stack.get_stack_metadata(render=render, stack=stack)
-
-        expected_resolution = serialem_template['resolutions'][stack]
-        delivered_resolution = [md.stackResolutionX, md.stackResolutionY, md.stackResolutionZ]
-
-        # test if resolution of stack is correct
-        assert (np.array(expected_resolution) - np.array(delivered_resolution) == [0, 0, 0]).all()
-
-        # cleanup
-        renderapi.stack.delete_stack(stack, render=render)
-
-    assert os.path.exists(example_serialem + '/conv_log')
-    with open(os.path.join(example_serialem, 'conv_log', os.listdir(example_serialem + '/conv_log')[0])) as file:
-        importlog = file.read()
-    assert importlog == serialem_template['errorlog0'] + example_serialem + serialem_template['errorlog1']
-
-    # cleanup
-    os.system('rm -rf ' + example_serialem)
-    renderapi.stack.delete_stack(stacks0[0], render=render)
+#
+# @pytest.mark.dependency()
+# def test_generate_SBEM(render):
+#
+#     assert isinstance(render, renderapi.render.Render)
+#
+#     ex = copy.deepcopy(generate_EM_tilespecs_from_SBEMImage.example_input)
+#     ex['render'] = render.make_kwargs()
+#
+#     with tempfile.NamedTemporaryFile(suffix='.json') as probablyemptyfn:
+#         outfile = probablyemptyfn.name
+#
+#     # test non existing directory
+#     ex['image_directory'] = example_sbem + 'notexistingdir'
+#
+#     with pytest.raises(ValidationError):
+#         mod1 = generate_EM_tilespecs_from_SBEMImage.GenerateSBEMImageTileSpecs(input_data=ex)
+#
+#     ex['image_directory'] = example_sbem
+#
+#     # test non existing "meta" directory
+#     os.rename(example_sbem + '/meta', example_sbem + '/meta123')
+#
+#     mod = generate_EM_tilespecs_from_SBEMImage.GenerateSBEMImageTileSpecs(input_data=ex)
+#
+#     with pytest.raises(FileNotFoundError):
+#         mod.run()
+#
+#     os.rename(example_sbem + '/meta123', example_sbem + '/meta')
+#
+#     # test missing tile image reporting to log
+#     os.remove(example_sbem + '/tiles/g0000/t0008/test_example_g0000_t0008_s00005.jpg')
+#
+#     mod.run()
+#
+#     assert os.path.exists(example_sbem + '/conv_log')
+#     with open(os.path.join(example_sbem, 'conv_log', os.listdir(example_sbem + '/conv_log')[0])) as file:
+#         importlog = file.read()
+#
+#     assert importlog == sbemimage_template['errorlog0'] + example_sbem + sbemimage_template['errorlog1']
+#
+#     expected_tileIds = set(sbemimage_template['tileids'])
+#     delivered_tileIds = set(renderapi.stack.get_stack_tileIds(ex['stack'], render=render))
+#
+#     # test if all tiles are imported
+#     assert len(expected_tileIds.symmetric_difference(delivered_tileIds)) == 0
+#
+#     md = renderapi.stack.get_stack_metadata(render=render, stack=ex['stack'])
+#
+#     expected_resolution = sbemimage_template['resolution']
+#     delivered_resolution = [md.stackResolutionX, md.stackResolutionY, md.stackResolutionZ]
+#
+#     # test if resolution of stack is correct
+#     assert (np.array(expected_resolution) - np.array(delivered_resolution) == [0, 0, 0]).all()
+#
+#     # this is needed for testing an export -> moved to 'test_materialize'
+#     # cleanup
+#     # os.system('rm -rf ' + example_sbem)
+#     # renderapi.stack.delete_stack(ex['stack'], render=render)
+#
+#
+# @pytest.mark.dependency()
+# def test_generate_SerialEM(render):
+#
+#     assert isinstance(render, renderapi.render.Render)
+#
+#     ex = copy.deepcopy(generate_EM_tilespecs_from_SerialEMmontage.example_input)
+#     ex['render'] = render.make_kwargs()
+#
+#     with tempfile.NamedTemporaryFile(suffix='.json') as probablyemptyfn:
+#         outfile = probablyemptyfn.name
+#
+#     # test non existing image file
+#     ex['image_file'] = os.path.join(example_serialem, 'notexistingfile')
+#
+#     with pytest.raises(ValidationError):
+#         mod1 = generate_EM_tilespecs_from_SerialEMmontage.GenerateSEMmontTileSpecs(input_data=ex)
+#
+#     ex['image_file'] = os.path.join(example_serialem, 'supermont.idoc')
+#
+#     mod = generate_EM_tilespecs_from_SerialEMmontage.GenerateSEMmontTileSpecs(input_data=ex)
+#
+#     stacks0 = mod.run()
+#
+#     assert len(stacks0) == 1
+#
+#     expected_stack0 = serialem_template['stack0']
+#
+#     assert stacks0[0] == expected_stack0
+#
+#     expected_tileIds = set(serialem_template['tileids0'])
+#
+#     delivered_tileIds = set(renderapi.stack.get_stack_tileIds(stacks0[0], render=render))
+#
+#     # test if all tiles are imported
+#     assert len(expected_tileIds.symmetric_difference(delivered_tileIds)) == 0
+#
+#     md = renderapi.stack.get_stack_metadata(render=render, stack=stacks0[0])
+#
+#     expected_resolution = serialem_template['resolution0']
+#     delivered_resolution = [md.stackResolutionX, md.stackResolutionY, md.stackResolutionZ]
+#
+#     # test if resolution of stack is correct
+#     assert (np.array(expected_resolution) - np.array(delivered_resolution) == [0, 0, 0]).all()
+#
+#     ex['image_file'] = os.path.join(example_serialem, 'mont01.idoc')
+#
+#     mod = generate_EM_tilespecs_from_SerialEMmontage.GenerateSEMmontTileSpecs(input_data=ex)
+#
+#     stacks = mod.run()
+#
+#     assert len(stacks) == 5
+#
+#     assert stacks == serialem_template['stacks']
+#
+#     for stack in stacks:
+#         expected_tileIds = set(serialem_template['tileids'][stack])
+#
+#         delivered_tileIds = set(renderapi.stack.get_stack_tileIds(stack, render=render))
+#
+#         # test if all tiles are imported
+#         assert len(expected_tileIds.symmetric_difference(delivered_tileIds)) == 0
+#
+#         md = renderapi.stack.get_stack_metadata(render=render, stack=stack)
+#
+#         expected_resolution = serialem_template['resolutions'][stack]
+#         delivered_resolution = [md.stackResolutionX, md.stackResolutionY, md.stackResolutionZ]
+#
+#         # test if resolution of stack is correct
+#         assert (np.array(expected_resolution) - np.array(delivered_resolution) == [0, 0, 0]).all()
+#
+#         # cleanup
+#         renderapi.stack.delete_stack(stack, render=render)
+#
+#     assert os.path.exists(example_serialem + '/conv_log')
+#     with open(os.path.join(example_serialem, 'conv_log', os.listdir(example_serialem + '/conv_log')[0])) as file:
+#         importlog = file.read()
+#     assert importlog == serialem_template['errorlog0'] + example_serialem + serialem_template['errorlog1']
+#
+#     # cleanup
+#     os.system('rm -rf ' + example_serialem)
+#     renderapi.stack.delete_stack(stacks0[0], render=render)
 
 @pytest.mark.dependency()
 def test_generate_TIF(render):
@@ -178,7 +179,7 @@ def test_generate_TIF(render):
     with tempfile.NamedTemporaryFile(suffix='.json') as probablyemptyfn:
         outfile = probablyemptyfn.name
 
-    # test non existing directory
+    # test non-existing directory
     ex['image_directory'] = example_tif + 'notexistingdir'
 
     with pytest.raises(ValidationError):
@@ -205,6 +206,8 @@ def test_generate_TIF(render):
     # test import
     ex['image_directory'] = example_tif
 
+    ex['autocrop'] = True
+
     mod3 = generate_EM_tilespecs_from_TIFStack.GenerateTifStackTileSpecs(input_data=ex)
     mod3.run()
 
@@ -213,9 +216,23 @@ def test_generate_TIF(render):
 
     # test if files have been renamed to "*.tif'
 
-    for imfile in os.listdir(example_tif):
+    o_tifs = os.listdir(example_tif)
+
+    #  test automatic cropping
+    assert 'autocrop' in o_tifs
+    assert os.path.isdir(os.path.join(example_tif, 'autocrop'))
+
+    o_tifs.remove('autocrop')
+
+    for imfile in o_tifs:
         assert os.path.splitext(imfile)[-1] == '.tif'
 
+    assert sorted(os.listdir(os.path.join(example_tif, 'autocrop'))) == sorted(o_tifs)
+
+    im0 = io.imread(os.path.join(example_tif, 'autocrop',tif_template['tileids'][0],'.tif'))
+    im1 = io.imread(os.path.join(example_tif, tif_template['tileids'][0],'.tif'))
+
+    assert (np.array(im1.shape) - np.array(im0.shape) == [1, 1]).all()
 
     # test if all tiles are imported
     assert len(expected_tileIds.symmetric_difference(delivered_tileIds)) == 0
@@ -229,5 +246,5 @@ def test_generate_TIF(render):
     assert (np.array(expected_resolution) - np.array(delivered_resolution) == [0, 0, 0]).all()
 
     # cleanup
-    os.system('rm -rf ' + example_tif)
+    # os.system('rm -rf ' + example_tif)
     renderapi.stack.delete_stack(ex['stack'], render=render)
