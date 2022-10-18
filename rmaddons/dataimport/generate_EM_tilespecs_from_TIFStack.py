4cebffb67559b886e7cc824de00ea84bf06ecf40
Martin Schorb <martin.schorb@embl.de>
2022-10-18 17:23:53 +0200

    modularize method and parallelize

--- rmaddons/dataimport/generate_EM_tilespecs_from_TIFStack.py
+++ rmaddons/dataimport/generate_EM_tilespecs_from_TIFStack.py
@@ -4,6 +4,7 @@ Create tilespecs from a directory containing aplhabetically ordered tif files
 """
 
 import os
+from functools import partial
 
 import tifffile
 
@@ -38,6 +39,8 @@ example_input = {
     "pool_size": 1,
     "close_stack": True,
     "z_index": 1,
+    "startidx": 0,
+    "endidx": -1,
     "output_stackVersion": {
         "stackResolutionX": 10.1
     }
@@ -56,6 +59,9 @@ class GenerateTifStackTileSpecs(StackOutputModule):
 
         """
 
+        poolclass = renderapi.client.WithPool
+        pool_size = self.args.get("pool_size")
+
         imgdir = os.path.realpath(imgdir)
 
         imfiles = glob.glob(os.path.join(imgdir, '*.[Tt][Ii][Ff]'))
@@ -66,81 +72,83 @@ class GenerateTifStackTileSpecs(StackOutputModule):
         if imfiles == []:
             raise FileNotFoundError('No TIF files found!')
 
-        tspecs = []
-
         resolution = self.args.get("pxs")[-1]  # in um
         autocrop = self.args.get("autocrop")
 
         transform = renderapi.transform.AffineModel(B0=0, B1=0)
 
-        for idx, imfile in enumerate(imfiles):
+        mypartial = partial(self.tiffile_to_ts, autocrop, imgdir, resolution, transform)
 
-            f1 = os.path.realpath(os.path.join(imgdir, imfile))
-
-            filepath = groupsharepath(f1)
-
-            basefile = os.path.splitext(filepath)[0]
-
-            # need to rename files to *.tif extension, otherwise render won't work
-            os.system('mv ' + filepath + ' ' + basefile + '.temp')
-            os.system('mv ' + basefile + '.temp ' + basefile + '.tif')
-
-            filepath = basefile + '.tif'
-
-            with TiffFile(filepath) as im:
-                width = im.pages.pages[0].imagewidth
-                height = im.pages.pages[0].imagelength
-                dtype = im.pages.pages[0].dtype.type
-                if autocrop:
-                    image = im.asarray()
-                    imcontent = np.argwhere(image != 0)  # assume blackground is zero
-                    min_x = imcontent[:, 0].min()
-                    max_x = imcontent[:, 0].max()
-                    min_y = imcontent[:, 1].min()
-                    max_y = imcontent[:, 1].max()
-
-
-                    imcrop = image[min_x:max_x+1, min_y:max_y+1]
-                    width = max_x - min_x
-                    height = max_y - min_y
-                    transform = renderapi.transform.AffineModel(B0=min_x, B1=min_y)
-                    fdir = os.path.dirname(filepath)
-                    if not os.path.exists(os.path.join(fdir,'autocrop')):
-                        os.mkdir(os.path.join(fdir,'autocrop'))
-
-                    filepath = os.path.join(fdir,'autocrop',os.path.basename(filepath))
-                    tifffile.imsave(filepath,imcrop)
-                    im.close()
-
-            ip = renderapi.image_pyramid.ImagePyramid()
-            ip[0] = renderapi.image_pyramid.MipMap(imageUrl='file://' + filepath)
-
-            slice = os.path.basename(os.path.splitext(imfile)[0])
-
-            slsplit = slice.split('_')
-
-            if slsplit[0] == 'slice' and slsplit[1].isnumeric():
-                idx = int(slsplit[1])
-
-            if idx % 50 == 0:
-                print("Importing " + slice + " for Render.")
-                print("\n...")
-
-            tspecs.append(renderapi.tilespec.TileSpec(
-                tileId=slice,
-                imagePyramid=ip,
-                z=idx,
-                width=width,
-                height=height,
-                minint=np.iinfo(dtype).min,
-                maxint=np.iinfo(dtype).max,
-                tforms=[transform],
-                sectionId=idx,
-                scopeId='TIFslice',
-                cameraId='TIFslice',
-                pixelsize=resolution))
+        with poolclass(pool_size) as pool:
+            tspecs = pool.map(mypartial, list(zip(imfiles, range(len(imfiles)))))
 
         return tspecs
+
+
+
+
+    def tiffile_to_ts(self, autocrop, imgdir, resolution, transform, inputkey):
+        imfile, idx = inputkey
+        f1 = os.path.realpath(os.path.join(imgdir, imfile))
+
+        filepath = groupsharepath(f1)
+        basefile = os.path.splitext(filepath)[0]
+
+        # need to rename files to *.tif extension, otherwise render won't work
+
+        os.system('mv ' + filepath + ' ' + basefile + '.temp')
+        os.system('mv ' + basefile + '.temp ' + basefile + '.tif')
+        filepath = basefile + '.tif'
+
+        with TiffFile(filepath) as im:
+            width = im.pages.pages[0].imagewidth
+            height = im.pages.pages[0].imagelength
+            dtype = im.pages.pages[0].dtype.type
+            if autocrop:
+                image = im.asarray()
+                imcontent = np.argwhere(image != 0)  # assume blackground is zero
+                min_x = imcontent[:, 0].min()
+                max_x = imcontent[:, 0].max()
+                min_y = imcontent[:, 1].min()
+                max_y = imcontent[:, 1].max()
+
+                imcrop = image[min_x:max_x + 1, min_y:max_y + 1]
+                width = max_x - min_x
+                height = max_y - min_y
+                transform = renderapi.transform.AffineModel(B0=min_x, B1=min_y)
+                fdir = os.path.dirname(filepath)
+                if not os.path.exists(os.path.join(fdir, 'autocrop')):
+                    os.mkdir(os.path.join(fdir, 'autocrop'))
+
+                filepath = os.path.join(fdir, 'autocrop', os.path.basename(filepath))
+                tifffile.imsave(filepath, imcrop)
+                im.close()
+
+        ip = renderapi.image_pyramid.ImagePyramid()
+        ip[0] = renderapi.image_pyramid.MipMap(imageUrl='file://' + filepath)
+        slice = os.path.basename(os.path.splitext(imfile)[0])
+        slsplit = slice.split('_')
+
+        if slsplit[0] == 'slice' and slsplit[1].isnumeric():
+            idx = int(slsplit[1])
+        if idx % 50 == 0:
+            print("Importing " + slice + " for Render.")
+            print("\n...")
+
+        ts = renderapi.tilespec.TileSpec(
+            tileId=slice,
+            imagePyramid=ip,
+            z=idx,
+            width=width,
+            height=height,
+            minint=np.iinfo(dtype).min,
+            maxint=np.iinfo(dtype).max,
+            tforms=[transform],
+            sectionId=idx,
+            scopeId='TIFslice',
+            cameraId='TIFslice',
+            pixelsize=resolution)
+        return ts
 
     def run(self):
         # with open(self.args['metafile'], 'r') as f:
