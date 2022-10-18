1b261ff97589a3b1ebfd5b56fd1f8abdd3f44ea0
Martin Schorb <martin.schorb@embl.de>
2022-10-18 09:47:10 +0200

    export class definitions

--- rmaddons/materialize/render_export_sections.py
+++ rmaddons/materialize/render_export_sections.py
@@ -36,9 +36,8 @@ class RenderSectionAtScale_extended(RenderSectionAtScale):
     default_schema = RenderSectionAtScale_extendedParameters
     default_output_schema = RenderSectionAtScaleOutput
 
-    @classmethod
     def downsample_specific_mipmapLevel(
-            cls, zvalues, input_stack=None, level=1, pool_size=1,
+            self, zvalues, input_stack=None, level=1, pool_size=1,
             image_directory=None, scale=None, imgformat=None,
             resolutionUnit=None, render=None, bounds=None,
             minInt=None, maxInt=None,
@@ -100,7 +99,7 @@ class RenderSectionAtScale_extended(RenderSectionAtScale):
 
             ds_source = temp_no_mipmap_stack
 
-        if cls.args.get("customPath"):
+        if self.args.get("customPath"):
             cOF = '.'
             cSF = '.'
         else:
