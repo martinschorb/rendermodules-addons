try to fix tile-specific rotation

rmaddons/dataimport/generate_EM_tilespecs_from_SBEMImage.py
@@ -109,17 +109,22 @@ class GenerateSBEMImageTileSpecs(StackOutputModule):
         ypos = float(tile['glob_y']) / pxs
         M = self.rotmatrix(rotation)
 
-        pos = np.dot(M.T, [xpos, ypos]) + np.dot(M.T,[width, height])
+        pos = np.dot(M.T, [xpos, ypos])
+        rotshift = np.dot(M.T, [height, width])
 
         tf_trans = renderapi.transform.AffineModel(
             B0=pos[0],
             B1=pos[1])
 
         tf_rot = renderapi.transform.AffineModel(
-                                  M00=M[0,0],
-                                  M01=M[0,1],
-                                  M10=M[1,0],
-                                  M11=M[1,1])
+            M00=M[0, 0],
+            M01=M[0, 1],
+            M10=M[1, 0],
+            M11=M[1, 1])
+
+        tf_rot_shift = renderapi.transform.AffineModel(
+            B0=rotshift[0],
+            B1=rotshift[1])
 
         print("Processing tile " + tile['tileid'] + " metadata for Render.")
 
@@ -130,7 +135,7 @@ class GenerateSBEMImageTileSpecs(StackOutputModule):
             width=tile['tile_width'],
             height=tile['tile_height'],
             minint=0, maxint=255,
-            tforms=[tf_trans, tf_rot],
+            tforms=[tf_rot, tf_trans],
             # imagePyramid=ip,
             sectionId=tile['slice_counter'],
             scopeId='3View',